d36e1149232c45557e4106fea270f235069f874d
Kosty <kosty.lygin.58@mail.ru>
2022-10-20 10:55:33 +0300

    Создание модели конференции

---
+++ ConvaReload.Domain/Entities/Conference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ConvaReload.Domain.Enum;
+
+namespace ConvaReload.Domain.Entities;
+
+public class Conference
+{
+    [Required] public string Name { get; set; }
+    [Required] public string Description { get; set; }
+    [Required] public DateTime Date { get; set; }
+    [Required] public string Organizer { get; set; }
+
+    [EmailAddress] public string? Email { get; set; }
+    [Phone] public string? Phone { get; set; }
+
+    [Required] public string Addition { get; set; }
+
+    [Required] public StatusConference status { get; set; }
+}
---
+++ ConvaReload.Domain/Enum/StatusConference.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ConvaReload.Domain.Enum;
+
+public enum StatusConference
+{
+    Expectation = 0,
+    Started = 1,
+}
