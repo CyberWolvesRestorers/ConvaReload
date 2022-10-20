197bca0f3f8afb2628a29fcb835bc4493e336349
sunshine <thesunshineofficial@gmail.com>
2022-10-20 15:56:08 +0300

    Fix conference entity

--- ConvaReload.Domain/Entities/Conference.cs
+++ ConvaReload.Domain/Entities/Conference.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using ConvaReload.Domain.Enum;
 
 namespace ConvaReload.Domain.Entities;
 
 public class Conference
 {
-    [Required] public string Name { get; set; }
-    [Required] public string Description { get; set; }
+    public int Id { get; set; }
+    [Required] public string Name { get; set; } = "";
+    [Required] public string Description { get; set; } = "";
     [Required] public DateTime Date { get; set; }
-    [Required] public string Organizer { get; set; }
-
+    [Required] public string Organizer { get; set; } = "";
     [EmailAddress] public string? Email { get; set; }
     [Phone] public string? Phone { get; set; }
-
-    [Required] public string Addition { get; set; }
-
-    [Required] public StatusConference status { get; set; }
+    [Required] public string Addition { get; set; } = "";
+    [Required] public StatusConference Status { get; set; }
 }
 
