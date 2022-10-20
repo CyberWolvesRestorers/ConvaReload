efa93f4b96faeb3126a0bf605c9b48306da5a532
sunshine <thesunshineofficial@gmail.com>
2022-10-20 16:05:11 +0300

    Migrate db

--- ConvaReload.DataAccess/Migrations/20221020130353_ConferenceInitial.Designer.cs
+++ ConvaReload.DataAccess/Migrations/20221020130353_ConferenceInitial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ConvaReload.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ConvaReload.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20221020130353_ConferenceInitial")]
+    partial class ConferenceInitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
